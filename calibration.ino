f0692270f167d4b2d542c8a26860bb0a6e1ca166
calebjjj <123593398+calebjjj@users.noreply.github.com>
2023-03-09 15:20:07 -0600

    Code for the operation of Spine-Kick V4

---
+++ calibration.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+void calibrate() {
+  // Get readings from gyroscope
+
+  // Store readings into a 3 lists
+  vector<float> X;
+  vector<float> Y;
+  vecotr<float> Z;
+  // Take the average of all values after 5 seconds and Standard deviation
+
+  // Store averages and std's and return them
+
+  return cal_X, cal_Y, cal_Z, offset_X, offset_Y, offset_Z
+}
