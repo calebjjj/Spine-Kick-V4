8daf29e1806bf93585c3f7f0b6bac459d6978a2f
calebjjj <123593398+calebjjj@users.noreply.github.com>
2023-03-30 22:02:07 -0500

    Update calibration.ino
    current version

--- calibration.ino
+++ calibration.ino
@@ -1,24 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600)
+#include <numeric>
+#include <vector>
 
+double average(vecotr<float> const& v) {
+  if(v.empty()) {
+    return 0;
+  }
+
+  auto const count = static_cast<float>(v.size());
+  return (reduce(v.begin(), v.end()) / count);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-}
 
-void calibrate() {
+void calibrate(roll_vect, pitch_vect, yaw_vect) {//could have it take the vectors as inputs instead) 
+
+  // Create 3 lists to store readings
+  vector<float> roll_vect = [];
+  vector<float> pitch_vect = [];
+  vecotr<float> yaw_vect [];
   // Get readings from gyroscope
+  for (int i = 0; i < 100; i++) {      
+    roll, pitch, yaw = read_acc();
 
-  // Store readings into a 3 lists
-  vector<float> X;
-  vector<float> Y;
-  vecotr<float> Z;
+    roll_vect.push_back(roll);
+    pitch_vect.push_back(pitch;)
+    yaw_vect.push_back(yaw);
+
+    delay(100)
+}
+
   // Take the average of all values after 5 seconds and Standard deviation
-
+  cal_roll = average(roll_vect);
+  cal_pitch = average(pitch_vect);
+  cal_yaw = average(yaw_vect);
   // Store averages and std's and return them
 
-  return cal_X, cal_Y, cal_Z, offset_X, offset_Y, offset_Z
+  return cal_roll, cal_pitch, cal_yaw
 }
